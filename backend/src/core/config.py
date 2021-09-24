67386d25c2224d94dff39d98028f18d0bdd533d6
Dragonprod <artyom2002-98@mail.ru>
2021-09-24 23:43:15 +0300

    add jwt template

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -2,3 +2,6 @@ import os
 
 PROJECT_NAME = os.getenv('PROJECT_NAME', 'LifeTech Hack Backend')
 API_V1_PREFIX = '/api/v1'
+
+JWT_SECRET = 'kUVwSgryxDI5vR9pDEYOQAqukdFZzXV1s6swUXJlgQxK96oI81LPOCgwB5VRLbmQ'
+JWT_ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24  * 7
---
+++ backend/src/crud/user.py
@@ -0,0 +1,14 @@
+from ..database.mongodb import AsyncIOMotorClient
+
+from ..core.config import database_name, users_collection_name
+from ..models.user import UserInCreate, UserInDb, UserInLogin
+from ..helpers.exceptions import EntityDoesNotExist
+
+async def create_user(conn: AsyncIOMotorClient, user: UserInCreate) -> UserInDb:
+    user = UserInDb(**user.dict())
+    user.password = user.encode_password(user.password)
+    result = await conn[database_name][users_collection_name].insert_one(user.dict())
+    return UserInDb(**user.dict())
+
+async def find_user_by_email(conn: AsyncIOMotorClient, email: str) -> UserInDb:
+    raise NotImplementedError
